08a378718dba00643af1a50a7375921c2a4db958
Abhidha Gupta <abhidha2105@gmail.com>
2026-08-07 20:25:47 +0530

    QR - Advance updates

--- 01_QR-Generator/04_Advanced.py
+++ 01_QR-Generator/04_Advanced.py
@@ -1,15 +1,21 @@
 import qrcode
 from PIL import Image
+import os
 
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H, # type: ignore
-                   box_size=10, border=10)
+                   box_size=7, border=4)
 
-# qr.add_data("Abhidha Gupta")
-qr.add_data("https://github.com/AGupta-23")
-# qr.add_data("9098977388")
-# qr.add_data("abhidha2105@gmail.com")
 
+qr.add_data("https://github.com/AGupta-23/Python-Projects/tree/main/01_QR-Generator")
 qr.make(fit=True)
 img = qr.make_image(fill_color="red", back_color="black")
-img.save("Advance-Github.png") # type: ignore
+
+file_path = os.path.abspath(__file__)
+print(file_path)
+
+directory_path = os.path.dirname(file_path)
+print(directory_path)
+
+loc = os.path.join(directory_path, "Advance-Github.png")
+img.save(loc) # type: ignore
